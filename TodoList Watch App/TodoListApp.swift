cb73d21b5b77702e2e16373543e576bd1589cc29
Deividson Sabino <xdeividsabinox@live.com>
2024-10-27 22:34:01 -0300

    initial commit

---
+++ TodoList Watch App/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList Watch App
+//
+//  Created by Deividson Sabino on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
