baa127399472cb685ca646b6360ad878eef8c957
Deividson Sabino <xdeividsabinox@live.com>
2024-10-28 00:07:05 -0300

    feat(ContentView.swift) create ui

--- TodoList Watch App/Views/ContentView.swift
+++ TodoList Watch App/Views/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = TaskViewModel()
+    @State private var newTaskTitle = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(viewModel.tasks) { task in
+                HStack {
+                    Text(task.title)
+                        .font(.headline)
+                        .strikethrough(task.isCompleted, color: .gray)
+                    Spacer()
+                    Button(action : {
+                        if let index = viewModel.tasks.firstIndex(where: { $0.id == task.id}) {
+                            viewModel.tasks[index].isCompleted.toggle()
+                        }
+                    }) {
+                        Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+                    }
+                }
+            }.onDelete(perform: viewModel.removeTask)
+            
+            if viewModel.tasks.isEmpty {
+                Text("No tasks yet").foregroundStyle(.gray)
+            }
         }
-        .padding()
+        VStack {
+            TextFieldLink {
+                Label("Add task", systemImage: "plus.circle.fill")
+                
+            } onSubmit: { newTitle in
+                viewModel.addTask(title: newTitle )
+            }
+        }.navigationTitle("Tasks")
     }
 }
 
